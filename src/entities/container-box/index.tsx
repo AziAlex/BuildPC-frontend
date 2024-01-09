9231db25304c937c02321a8c65b41b346bb22c27
menyaboo <tosha.kolmay@gmail.com>
2024-01-09 21:32:12 +0700

    pull request fixed problems

--- src/entities/container-box/index.tsx
+++
@@ -1,9 +0,0 @@
-import { ContainerBoxStyled } from '@/entities/container-box/style.ts'
-
-type Props = {
-  children: React.ReactNode
-}
-
-export const ContainerBox = (props: Props) => (
-  <ContainerBoxStyled>{props.children}</ContainerBoxStyled>
-)
--- src/pages/main-page/index.tsx
+++ src/pages/main-page/index.tsx
@@ -1,30 +1,11 @@
 import { NavAccessories } from '@/widgets/nav-accessories'
-import { ContainerBox } from '@/entities/container-box'
-import styled from 'styled-components'
+import { Main } from '@/widgets/main'
 
-export const MainPageWrapper = styled.div`
-  height: 100%;
-  display: flex;
-  gap: 40px;
-`
-
-export const GridContainersBoxes = styled.div`
-  display: grid;
-  width: 100%;
-  grid-template-columns: 3fr 1fr;
-  grid-template-rows: repeat(2, 1fr);
-  gap: 40px;
-`
+import { MainPageWrapper } from '@/pages/main-page/style.ts'
 
 export const MainPage = () => (
   <MainPageWrapper>
     <NavAccessories />
-    <GridContainersBoxes>
-      {[...Array(4)].map(() => (
-        <ContainerBox>
-          <></>
-        </ContainerBox>
-      ))}
-    </GridContainersBoxes>
+    <Main />
   </MainPageWrapper>
 )
