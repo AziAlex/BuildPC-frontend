7ccee86fc50fcdc11b07abac408dd8d9bc2c5adc
menyaboo <tosha.kolmay@gmail.com>
2024-01-09 13:31:55 +0700

    add alias @public, bg to App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,14 +2,14 @@ import { MainPage } from '@/pages/main-page'
 import { AppProviders } from './providers/AppProviders.tsx'
 import GlobalStyles from '@/app/styles/global.ts'
 import styled from 'styled-components'
+import bg from '@public/background.png'
 
 export const AppWrapper = styled.div`
   width: 100%;
   height: 100svh;
   position: relative;
-  background-color: ${({ theme }) => theme.colors.bg};
   color: ${({ theme }) => theme.colors.text};
-  overflow: hidden;
+  background: url(${bg}) center center / cover no-repeat;
   padding: 40px 40px 40px 0;
 `
 
