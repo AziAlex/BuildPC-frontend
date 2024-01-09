f22d44535f60b75b8f2e593027f96ed9aa8b1038
menyaboo <tosha.kolmay@gmail.com>
2024-01-09 16:06:13 +0700

    add layout app

--- src/pages/main-page/index.tsx
+++ src/pages/main-page/index.tsx
@@ -1,7 +1,30 @@
 import { NavAccessories } from '@/widgets/nav-accessories'
+import { ContainerBox } from '@/entities/container-box'
+import styled from 'styled-components'
+
+export const MainPageWrapper = styled.div`
+  height: 100%;
+  display: flex;
+  gap: 40px;
+`
+
+export const GridContainersBoxes = styled.div`
+  display: grid;
+  width: 100%;
+  grid-template-columns: 3fr 1fr;
+  grid-template-rows: repeat(2, 1fr);
+  gap: 40px;
+`
 
 export const MainPage = () => (
-  <>
+  <MainPageWrapper>
     <NavAccessories />
-  </>
+    <GridContainersBoxes>
+      {[...Array(4)].map(() => (
+        <ContainerBox>
+          <></>
+        </ContainerBox>
+      ))}
+    </GridContainersBoxes>
+  </MainPageWrapper>
 )
