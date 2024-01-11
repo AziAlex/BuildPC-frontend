ba04ebeffedab26c1f9e63cf085ab18210488bd4
menyaboo <tosha.kolmay@gmail.com>
2024-01-12 03:54:33 +0700

    add fake data currentAccessory.ts

---
+++ src/shared/data/currentAccessory.ts
@@ -0,0 +1,19 @@
+import { IAccessoryItem } from '@/shared/types/IAccessoryItem.ts'
+
+export const currentAccessory: IAccessoryItem = {
+  _id: '123',
+  type: 'processory',
+  marketplace: 'cityLink',
+  name: 'Процессор Intel Core i3 10105, LGA 1200,  OEM [cm8070104291321s rh3p]',
+  price: 10990,
+  href: 'https://www.citilink.ru/product/processor-intel-s-core-i3-10105-soc-1200-3-7ghz-iuhdg630-oem-1722987/',
+  characteristics: [
+    'Ядро Comet Lake Refresh;',
+    'Частота 3.7 ГГц и 4.4 в режиме Turbo;',
+    'Сокет LGA 1200;',
+    'Число ядер 4, потоков 8;',
+    'Тепловыделение 65 Вт;',
+    'Техпроцесс 14 нм;',
+  ],
+  img: 'https://cdn.citilink.ru/Mx9_id88fGN6LUwtqn56rJ_tkHWfVWrifwU9xfR7TfQ/enlarge:1/gravity:ce/height:450/resizing_type:fit/width:450/plain/product-images/0e281141-b791-49ab-a71c-c3f71d9cc22c.jpg',
+}
