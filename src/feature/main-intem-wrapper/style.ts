e0a3e13a61ce05b130dd044c47c44feb3c1393a7
menyaboo <tosha.kolmay@gmail.com>
2024-01-12 03:58:39 +0700

    add scroll option main-item-wrapper component

--- src/feature/main-intem-wrapper/style.ts
+++ src/feature/main-intem-wrapper/style.ts
@@ -1,9 +1,10 @@
 import styled from 'styled-components'
 
-export const ContainerBoxStyled = styled.div`
+export const ContainerBoxStyled = styled.div<{ $isScroll?: boolean }>`
   width: 100%;
   height: 100%;
   border-radius: 30px;
   background: rgba(255, 255, 255, 0.1);
   box-shadow: 0 4px 30px 0 rgba(0, 0, 0, 0.06);
+  padding: ${(props) => (props.$isScroll ? '40px 40px 0 40px' : '40px')};
 `
